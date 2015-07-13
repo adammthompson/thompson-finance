a0db2a002733a830301fed86b646f617734b06f4
adammthompson <adammilesthompson@gmail.com>
2015-07-13 16:51:11 -0700

    Initial.

---
+++ cci.m
@@ -0,0 +1,58 @@
+# Copyright (C) 2015 Adam Thompson <adammilesthompson [at] gmail [dot] com>
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, see <http://www.gnu.org/licenses/>.
+
+# -*- texinfo -*-
+# @deftypefn {Function File} {[@var{ccindex}] =} cci (@var{asset, period, constant})
+#
+# Calculate the Commodity Channel Index (CCI) of an asset from the matrix whose
+# columns are high, low, and closing prices (@var{asset}).
+#
+# The CCI is calculated as the typical price minus the @var{period} simple
+# moving average of the typical price, all divided by the mean deviation of the
+# typical price. The typical price for each day is the average of the high,
+# low, and closing prices.
+#
+# With a @var{constant} of 0.015, approximately 70 to 80 percent of CCI values
+# will typically fall between -100 and +100.
+#
+# Dependencies:
+# Octave statistics package,
+# movavg function ().
+# @end deftypefn
+
+
+function ccindex = cci (asset, period, constant)
+# CCI Calculate Commodity Channel Index.
+
+# cci (asset, period, constant) = Calculate commodity channel index. asset
+# is 3-column matrix (columns are high, low, & close). If constant = 0.015,
+# ~75# of values will be [-100, 100].
+
+pkg load statistics;
+
+m = rows (asset);
+ccindex = zeros (m, 1);
+typ_price = sum (asset, 2) ./ 3;
+
+cci_sma = movavg (typ_price, period, 1);
+
+mean_dev = zeros (m, 1);
+for i = period: m
+	mean_dev(i) = mad (typ_price(i - period + 1: i));
+end
+ccindex = (typ_price - cci_sma) ./ (constant * mean_dev);
+ccindex (1: period - 1) = zeros(period - 1, 1);
+
+end
---
+++ movavg.m
@@ -0,0 +1,56 @@
+## Copyright (C) 2015 Adam Thompson <adammilesthompson [at] gmail [dot] com>
+##
+## This program is free software; you can redistribute it and/or modify it under
+## the terms of the GNU General Public License as published by the Free Software
+## Foundation; either version 3 of the License, or (at your option) any later
+## version.
+##
+## This program is distributed in the hope that it will be useful, but WITHOUT
+## ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+## FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+## details.
+##
+## You should have received a copy of the GNU General Public License along with
+## this program; if not, see <http://www.gnu.org/licenses/>.
+
+## -*- texinfo -*-
+# @deftypefn {Function File} {[@var{ma}] =} movavg (@var{asset, period, variant})
+#
+# Calculate the moving average of an (@var{asset}).
+#
+# If @var{variant} is 1, calculate the simple moving average. If @var{variant}
+# is 2, calculate the exponential moving average. A simple moving average is
+# just the average price of the @var{asset} over a specific number of periods.
+# The exponential moving average applies more weight to recent prices. The
+# weighting applied to the most recent price depends on the number of periods:
+# a factor of 2 divided by the number of periods + 1 is used.
+#
+# For the simple moving average, the first @var{period} values are filled with
+# zeros.
+#
+# Dependencies:
+# none.
+# @end deftypefn
+
+
+function ma = movavg (asset, period, variant)
+# MOVAVG Create moving average.
+# ma = MOVAVG (asset, period, variant) creates column vector of	moving
+# average. Inputs are column vector of asset and period for moving average.
+# If variant is 1, calculate the simple moving average. If variant is 2,
+# calculate the exponential moving average.
+#
+# Formulas for calculating moving averages provided by James Sherman, Jr.
+# http://octave.1599824.n4.nabble.com/vectorized-moving-average-td2132090.html
+
+if variant == 1
+	ma = filter ((1 / period) * ones(1, period), 1, asset);
+	ma(1: period - 1) = zeros(period - 1, 1);
+elseif variant == 2
+	alpha = 2 / (period + 1);
+	ma = filter (alpha, [1 (alpha - 1)], asset, asset(1) * (1 - alpha));
+else
+	error ('variant must be 1 or 2.')
+end
+
+end

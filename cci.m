c0c102bd1435469e04ff711fe05b6eeee38c15c6
Adam Thompson <adam@i3-adam>
2019-07-27 19:11:00 -0700

    Vectorized code and sped up by ~13,000%. Also removed statistics package dependency.

--- cci.m
+++ cci.m
@@ -1,4 +1,4 @@
-% Copyright (C) 2015 Adam Thompson <adammilesthompson [at] gmail [dot] com>
+% Copyright (C) 2019 Adam Thompson <adammilesthompson [at] gmail [dot] com>
 %
 % This program is free software; you can redistribute it and/or modify it under
 % the terms of the GNU General Public License as published by the Free Software
@@ -14,7 +14,7 @@
 % this program; if not, see <http://www.gnu.org/licenses/>.
 
 % -*- texinfo -*-
-% @deftypefn {Function File} {[@var{ccindex}] =} cci (@var{asset, period, constant})
+% @deftypefn {Function File} {[@var{ccindex}] =} cci(@var{asset, period, constant})
 %
 % Calculate the Commodity Channel Index (CCI) of an asset from the matrix whose
 % columns are high, low, and closing prices (@var{asset}).
@@ -33,40 +33,40 @@
 % The beginning of the CCI is padded with NANs to match the size of @var{asset}.
 %
 % Dependencies:
-% Octave statistics package,
 % m_average function.
 % @end deftypefn
 
 
-function ccindex = cci (asset, period = 20, constant = 0.015)
+function ccindex = cci(asset, period = 20, constant = 0.015)
 % CCI Calculate Commodity Channel Index.
 
-% cci (asset, period, constant) = Calculate commodity channel index. asset
+% cci(asset, period, constant) = Calculate commodity channel index. asset
 % is 3-column matrix (columns are high, low, & close). If constant = 0.015,
 % ~75% of values will be [-100, 100].
 
 pkg load statistics;
 
 if nargin < 1
-	printf ('Error: must have at least one argument.')	
+	printf('Error: must have at least one argument.')	
 elseif nargin > 3
-	printf ('Error: must have at most three arguments.')
-elseif period > rows (asset)
-	error ('Error: period must be <= the number of rows in asset matrix')
-elseif ! ismatrix (asset)
-	error ('Error: asset must be a matrix')
+	printf('Error: must have at most three arguments.')
+elseif period > rows(asset)
+	error('Error: period must be <= the number of rows in asset matrix')
+elseif !ismatrix(asset)
+	error('Error: asset must be a matrix')
 end
 
-m = rows (asset);
-ccindex = nan (m, 1);
-typ_price = mean (asset, 2);
+m = rows(asset);
+ccindex = nan(m, 1);
+typ_price = mean(asset, 2);
 
-cci_sma = m_average (typ_price, period, 1);
+cci_sma = m_average(typ_price, period, 1);
 
-mean_dev = zeros (m, 1);
-for i = period: m
-	mean_dev(i) = mad (typ_price (i - period + 1: i));
-end
+mean_dev = zeros(m, 1);
+
+sma_rep = repmat(cci_sma, 1, period);
+idx = [1: m - period + 1]' + [0: period - 1];
+mean_dev(period: m) = mean(abs(typ_price(idx) - sma_rep(period: m, :)), 2);
 
 ccindex = (typ_price - cci_sma) ./ (constant * mean_dev);
 
